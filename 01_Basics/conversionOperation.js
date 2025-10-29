30fcc256abee415f9252c4f2edbfdef4c09eeb23
AnveshTomarDEV <166391456+AnveshTomarDEV@users.noreply.github.com>
2025-10-29 11:50:08 +0000

    basics

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -66,3 +66,25 @@ console.log(gameCounter);
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
